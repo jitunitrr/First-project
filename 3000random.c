a7958b9fc6a739da212045dbdad35f758f0afd9a
Jitendra kumar singh <jitunitrr@gmail.com>
2017-10-24 00:41:49 +0530

    Add files via upload

---
+++ 3000random.c
@@ -0,0 +1,80 @@
+//3000random.c 
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+#define BUFSIZE 1024
+
+typedef struct rand_state {
+        unsigned long buffer[BUFSIZE];
+        int current;
+        int fd;
+} rand_state;
+
+void fill_rand_buffer(rand_state *r)
+{
+        ssize_t count;
+
+        count = read(r->fd, (void *) &r->buffer,
+                     BUFSIZE * sizeof(unsigned long));
+
+        if (count > sizeof(unsigned long)) {
+                r->current = count % sizeof(unsigned long);
+        } else {
+                fprintf(stderr, "Couldn't fill random buffer.\n");
+        }
+}
+
+void system_rand_init(rand_state *r)
+{
+        r->fd = open("/dev/urandom", O_RDONLY);
+
+        fill_rand_buffer(r);
+}
+
+long system_rand(long max, rand_state *r)
+{        
+        unsigned long val;
+
+        if (r->current < 0) {
+                fill_rand_buffer(r);                
+                if (r->current < 0) {
+                        /* fill_rand_buffer should have already
+                           reported an error */
+                        return 0;
+                }
+        }
+
+        val = r->buffer[r->current];
+        r->current--;
+        return val % max;
+}
+
+int main(int argc, char *argv[])
+{
+        int count, i;
+        long max, x;
+        rand_state r;
+
+        if (argc != 3) {
+                fprintf(stderr, "Usage: %s <count> <max>\n", argv[0]);
+                exit(-1);
+        }
+
+        count = atoi(argv[1]);
+        max = atol(argv[2]);
+
+        printf("count = %d, max = %ld\n", count, max);
+        
+        system_rand_init(&r);
+        
+        for (i = 0; i < count; i++) {
+                x = system_rand(max, &r);
+                printf("%ld\n", x);
+        }
+        
+        return 0;
+}
