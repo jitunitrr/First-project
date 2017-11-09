f97fa5319f9708af752b8c792ff6e73663c80aca
Jitendra kumar singh <jitunitrr@gmail.com>
2017-11-09 14:41:37 +0530

    Update Polygon.java

--- Polygon.java
+++ Polygon.java
@@ -1,11 +1,10 @@
 
-public class Polygon {
-	double verticesX[];
-	double verticesY[];
-	int numberOfVertices;
+class Polygon {
+	private double verticesX[];
+	private double verticesY[];
+	private int numberOfVertices;
 	public Polygon() {}
 	public Polygon(double[] verticesX, double[] verticesY) {
-		super();
 		this.verticesX = verticesX;
 		this.verticesY = verticesY;
 	}
@@ -20,15 +19,35 @@ public class Polygon {
 		this.verticesX = verticesX;
 		this.verticesY = verticesY;
 	}
-int getNVertices(){
-	return numberOfVertices;
-}
-public String toString(){
-	String XY="";
-	for(int i=0;i<verticesX.length;i++){
-		XY=XY+"("+verticesX[i]+","+verticesY[i]+") \n";
+	int getNVertices(){
+		return numberOfVertices;
 	}
-		return XY;
+	public double area(){
+		double calc=0;
+		int N=verticesX.length;
+		for(int i=0;i<verticesX.length;i++){
+			calc=calc+((verticesX[i%N] * verticesY[(i+1)%N])-(verticesX[(i+1)%N] * verticesY[i%N]));
+		}
+		calc=calc/2;
+		calc=Math.abs(calc);
+		return calc;
+	}
+	public double perimeter(){
+		double calc=0;
+		int N=verticesX.length;
+		for(int i=0;i<verticesX.length;i++){
+            double xx1=(verticesX[i%N]-verticesX[(i+1)%N] )* (verticesX[i%N]-verticesX[(i+1)%N]);
+            double yy1=(verticesY[(i)%N] * verticesY[(i+1)%N]) * (verticesY[(i)%N] * verticesY[(i+1)%N]);
+			calc=calc+Math.sqrt(xx1+yy1);
+		}
+		return calc;
+	}
+	public String toString(){
+	String XY="";
+		for(int i=0;i<verticesX.length;i++){
+			XY=XY+"("+verticesX[i]+","+verticesY[i]+") \n";
+		}
+	return XY;
 	
-}
+	}
 }
